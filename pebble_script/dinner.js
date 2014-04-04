2176711160c70a6a1861365699de80a140927146
Chris Gervang <gervangster@live.com>
2014-04-04 00:18:50 -0700

    pebble script 2

--- pebble_script/dinner.js
+++ pebble_script/dinner.js
@@ -2,7 +2,7 @@ simply.title('Clyde@Home');
 simply.subtitle('dinner?');
 simply.on('singleClick', function(e) {
   if (e.button === 'select') {
-    ajax({ url: 'localhost:8000/trigger/dinner' }, function(data){
+    ajax({ url: 'http://localhost:8000/trigger/dinner' }, function(data){
 	  simply.subtitle(data);
 	});
   } 
