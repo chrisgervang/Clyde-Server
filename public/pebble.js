4233f7566e8b82294e54e56fdc60a7434ccbda89
Chris Gervang <gervangster@live.com>
2014-04-17 00:33:44 -0700

    pebble script is now local

---
+++ public/pebble.js
@@ -0,0 +1,11 @@
+simply.title('Clyde');
+simply.subtitle('sleep?');
+simply.on('singleClick', function(e) {
+  if (e.button === 'select') {
+    ajax({ url: 'http://192.168.0.110:8000/trigger/sleep' }, function(data){
+	  simply.subtitle(data);
+	});
+  } 
+});
+
+
