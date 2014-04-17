6b3d4b10d0508733f5bf87e89ca0678525636610
Chris Gervang <gervangster@live.com>
2014-04-17 00:29:04 -0700

    demo day pebble change

--- pebble_script/dinner.js
+++ pebble_script/dinner.js
@@ -2,7 +2,7 @@ simply.title('Clyde');
 simply.subtitle('sleep?');
 simply.on('singleClick', function(e) {
   if (e.button === 'select') {
-    ajax({ url: 'http://192.168.0.110:8000/demo/sleep' }, function(data){
+    ajax({ url: 'http://192.168.0.110:8000/trigger/sleep' }, function(data){
 	  simply.subtitle(data);
 	});
   } 
